1a518253203366afa19724a9329bc33f13b3e6ef
Ewan Mellor <ewan@tipbit.com>
2015-03-28 12:10:13 -0700

    Added KissXML.framework, and tidy up the project.

---
+++ KissXML.framework/KissXML.framework.h
@@ -0,0 +1,19 @@
+//
+//  KissXML.framework.h
+//  KissXML.framework
+//
+//  Created by Ewan Mellor on 3/28/15.
+//  Copyright (c) 2015 Robbie Hanson with modifications from Tipbit, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KissXML.framework.
+FOUNDATION_EXPORT double KissXML_frameworkVersionNumber;
+
+//! Project version string for KissXML.framework.
+FOUNDATION_EXPORT const unsigned char KissXML_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KissXML_framework/PublicHeader.h>
+
+
