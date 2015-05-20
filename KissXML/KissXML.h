4a60959f5d0c61e9a2319e4b8c69d872010b3764
Friedrich Graeter <graeter@hydrixos.net>
2015-05-20 17:13:00 +0200

    Add Framework to KissXML

---
+++ KissXML/KissXML.h
@@ -0,0 +1,19 @@
+//
+//  KissXML.h
+//  KissXML
+//
+//  Created by Friedrich Gräter on 20/05/15.
+//  Copyright (c) 2015 Robbie Hanson with modifications from Tipbit, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KissXML.
+FOUNDATION_EXPORT double KissXMLVersionNumber;
+
+//! Project version string for KissXML.
+FOUNDATION_EXPORT const unsigned char KissXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KissXML/PublicHeader.h>
+
+
